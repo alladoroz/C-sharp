4e8a8f87afd3df92f8db860c9dadbb2dd3c4e0ae
alladoroz <alla.dorozhkina@gmail.com>
2014-04-14 19:42:22 +0400

    HashTable

---
+++ Hash/Hash-table/MyHashTable.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hash_table
+{
+    public class MyHashTable<T>
+    {
+        private List<T>[] table;
+        private const int tablesize = 1000;
+
+        public MyHashTable()
+        {
+            this.table = new List<T>[tablesize];
+        }
+
+        private int getHash(T element)
+        {
+            return element.GetHashCode() % tablesize;
+        }
+
+        public void add(T element)
+        {
+            int hash = getHash(element);
+            if (table[hash] == null)
+                table[hash] =  new List<T>();
+            table[hash].Add(element);
+
+        }
+
+        public bool contain(T element) 
+        {
+            return table[getHash(element)].Contains(element);
+        }
+
+        public void delete(T element) 
+        {
+            if (table[getHash(element)] == null)
+                return;
+
+            if (!table[getHash(element)].Contains(element))
+                return;
+
+            table[getHash(element)].Remove(element);
+        }
+    }
+}
---
+++ Hash/MyHashTable.Test/MyHashTable.Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hash_table;
+
+namespace MyHashTable.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            MyHashTable<String> test = new MyHashTable<String>();
+            test.add("ololo");
+            Assert.IsTrue(test.contain("ololo"));
+        }
+    }
+}
